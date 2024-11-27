22b3455e18313ce1bc7a5861e071356c4210001b
AMS-JR <asj.sarjo@gmail.com>
2024-11-27 16:49:33 -0600

    commit 1/2 of problem1

--- src/main/java/problem1/Customer.java
+++ src/main/java/problem1/Customer.java
@@ -1,7 +1,64 @@
 package problem1;
 
 public class Customer {
-    public static void main(String[] args) {
-        System.out.println("Customer");
+    private String firstName;
+    private String lastName;
+    private String socSecurityNum;
+    Address billingAddress;
+    Address shippingAddress;
+    //Initialize billingAddress
+    //and shippingAddress through its setter from Customer class
+    //Constructor
+    public Customer(String firstName, String lastName, String socSecurityNum){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socSecurityNum = socSecurityNum;
+    }
+    //Create getter, and setter methods for all five attributes.
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Address getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(Address billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    public String getSocSecurityNum() {
+        return socSecurityNum;
+    }
+
+    public void setSocSecurityNum(String socSecurityNum) {
+        this.socSecurityNum = socSecurityNum;
+    }
+
+    public Address getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(Address shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + firstName + ',' + lastName + ',' +
+                "ssn:" + socSecurityNum + ']';
     }
 }
