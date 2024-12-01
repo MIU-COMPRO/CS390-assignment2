069dbffc75837fbd58008e9cd127c62d0bdaba1b
Amadou Sarjo Jallow <asj.sarjo@gmail.com>
2024-12-01 16:34:30 -0600

    Another Commit problem_four

--- src/main/java/problem_four/Rectangle.java
+++ src/main/java/problem_four/Rectangle.java
@@ -1,13 +1,13 @@
 package problem_four;
 
-final public class Rectangle {
+public final class Rectangle {
     private final double length;
     private final double width;
 
     public Rectangle(double width, double length){
 
         if(width <= 0.0 || length <= 0.0 ){
-            throw new IllegalArgumentException("width and radius should be positive values");
+            throw new IllegalArgumentException("width and length should be positive values");
         }
         this.length = length;
         this.width = width;
